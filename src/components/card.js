20ad104cce232422f36688bfb68014838db0591e
Michael Spohn <mikeLspohn@gmail.com>
2016-10-30 13:06:47 -0500

    Cards for markets getting closer

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,9 @@ const Card = (props) => (
   <div className='card'>
     <div className='card-block'>
       <h4 className='card-title'>{props.title}</h4>
-      {props.textItems.map(item => <p className='card-text'>{item}</p>)}
+      {props.textItems.map(item => (
+        <p key={Math.random()} className='card-text'>{item}</p>
+      ))}
     </div>
   </div>
 )
