bb819b3b2e3bb85bf97aef9efd2d4e0bfa38c95e
Michael Spohn <mikeLspohn@gmail.com>
2016-10-29 22:14:30 -0500

    Navbar (#4)
    * Get basic navbar setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './navbar'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className='row'>
+        <Navbar links={['Sign In', 'Contact']} />
+      </div>
     );
   }
 }
