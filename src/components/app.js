cede1ede6ffc242bfcf7bb3a50bc520707998098
Michael Spohn <mikeLspohn@gmail.com>
2016-10-30 12:24:44 -0500

    Add market details in card

--- src/components/app.js
+++ src/components/app.js
@@ -5,30 +5,63 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {markets: []}
+    this.state = {markets: [], marketDetails: []}
   }
 
   componentDidMount() {
     const baseURL = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/'
     const zip = '67208';
 
+    const self = this;
     $.ajax({
       type: "GET",
       contentType: "application/json; charset=utf-8",
       url: `${baseURL}/zipSearch?zip=${zip}`,
       dataType: 'jsonp',
-      success: (data) => this.setState({markets: data.results}),
+      success: (data) => {
+        this.setState({markets: data.results})
+        console.log(this.state)
+        const ids = this.state.markets.map(market => market.id)
+        ids.forEach(id => {
+
+        $.ajax({
+          type: 'GET',
+          contentType: 'application/json; charset=utf-8',
+          url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + id,
+          dataType: 'jsonp',
+          success: data => self.setState({marketDetails: self.state.marketDetails.concat(data)})
+        })
+
+        })
+      },
       fail: err => console.log(err)
     });
   }
 
   render() {
+    const details = () => {
+      if(this.state.marketDetails.length) {
+        return this.state.marketDetails.map(market => {
+          console.log(market)
+          return (
+            <div className='card'>
+              <div className='card-block'>
+                <h4 className='card-title'>{market.marketdetails.Address}</h4>
+                <p className='card-text'>{market.marketdetails.GoogleLink}</p>
+                <p className='card-text'>{market.marketdetails.Products}</p>
+                <p className='card-text'>{market.marketdetails.Schedule}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+
+      return <p>No Details</p>
+    }
     return (
       <div className='row'>
         <Navbar links={['Sign In', 'Contact']} />
-        {this.state.markets.map(market => {
-          return <p key={market.id}>{market.marketname}</p>
-        })}
+        {details()}
       </div>
     );
   }
